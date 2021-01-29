047a35ac3635561f6f17f9d1ac001e8391045a75
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-01-28 18:40:32 -0800

    download spring boot project

---
+++ src/main/java/com/capstone/habitbuilder/HabitBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.habitbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/habitbuilder/HabitBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.habitbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
