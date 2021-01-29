e5f150f8791501641223da874a0f784c2daa9053
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-01-28 20:39:55 -0800

    drafted controller for habit and user

--- src/main/java/com/capstone/habitbuilder/habit/HabitController.java
+++ src/main/java/com/capstone/habitbuilder/habit/HabitController.java
@@ -1,4 +1,63 @@
 package com.capstone.habitbuilder.habit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="/habit")
 public class HabitController {
+    private final HabitService habitService;
+
+    @Autowired
+    public HabitController(HabitService habitService) {
+        this.habitService = habitService;
+    }
+
+    // Index
+    @GetMapping
+    public Iterable<Habit> getHabits() {
+        return habitService.getHabits();
+    }
+
+    // Show
+    @GetMapping(path = "{habitId}")
+    public void showHabit(
+        @PathVariable("habitId") Long habitId,
+        @RequestParam(required = false) String title,
+        @RequestParam(required = false) String goal,
+        @RequestParam(required = false) String description,
+        @RequestParam(required = false) String streak,
+        @RequestParam(required = false) String reminder,
+        @RequestParam(required = false) String createdDate,
+        @RequestParam(required = false) String updatedDate) {
+
+        habitService.showHabit(habitId, title, goal, description, streak, reminder, createdDate, updatedDate);
+    }
+
+    // Create
+    @PostMapping
+    public void registerNewHabit(@RequestBody Habit habit) {
+        HabitService.addNewHabit(habit);
+    }
+
+    // Update
+    @PutMapping(path = "{habitId}")
+    public void updateStudent(
+            @PathVariable("habitId") Long habitId,
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) String goal,
+            @RequestParam(required = false) String description,
+            @RequestParam(required = false) String streak,
+            @RequestParam(required = false) String reminder,
+            @RequestParam(required = false) String createdDate,
+            @RequestParam(required = false) String updatedDate) {
+
+        habitService.updateHabit(habitId, title, goal, description, streak, reminder, createdDate, updatedDate);
+    }
+
+    // Delete
+    @DeleteMapping(path = "{habitId}")
+    public void deleteHabit(@PathVariable("habitId") Long habitId) {
+        habitService.deleteHabit(habitId);
+    }
 }
--- src/main/java/com/capstone/habitbuilder/user/UserController.java
+++ src/main/java/com/capstone/habitbuilder/user/UserController.java
@@ -1,4 +1,61 @@
 package com.capstone.habitbuilder.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="/user")
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Index
+    @GetMapping
+    public Iterable<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    // Show
+    @GetMapping(path = "{userId}")
+    public void showUser(
+            @PathVariable("userId") Long userId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String photoUrl,
+            @RequestParam(required = false) String about,
+            @RequestParam(required = false) String createdDate,
+            @RequestParam(required = false) String updatedDate) {
+
+        userService.showUser(userId, name, email, photoUrl, about, createdDate, updatedDate);
+    }
+
+    // Create
+    @PostMapping
+    public void registerNewUser(@RequestBody User user) {
+        UserService.addNewUser(user);
+    }
+
+    // Update
+    @PutMapping(path = "{userId}")
+    public void updateUser(
+            @PathVariable("userId") Long userId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String photoUrl,
+            @RequestParam(required = false) String about,
+            @RequestParam(required = false) String createdDate,
+            @RequestParam(required = false) String updatedDate {
+
+        userService.updateUser(userId, name, email, photoUrl, about, createdDate, updatedDate);
+    }
+
+    // Delete
+    @DeleteMapping(path = "{userId}")
+    public void deleteUser(@PathVariable("userId") Long userId) {
+        userService.deleteUser(userId);
+    }
 }
