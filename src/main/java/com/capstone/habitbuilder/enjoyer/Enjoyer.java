aa1b7bd5db0e049eba7a9cc3b48cfef03460b636
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-01-30 01:15:54 -0800

    built user's one-to-many relation with requester/reciever in friendship

--- src/main/java/com/capstone/habitbuilder/enjoyer/Enjoyer.java
+++ src/main/java/com/capstone/habitbuilder/enjoyer/Enjoyer.java
@@ -1,6 +1,7 @@
 package com.capstone.habitbuilder.enjoyer;
 
 import com.capstone.habitbuilder.Auditable;
+import com.capstone.habitbuilder.friendship.Friendship;
 import com.capstone.habitbuilder.habit.Habit;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -9,7 +10,9 @@ import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Setter(AccessLevel.PUBLIC)
 @Getter(AccessLevel.PUBLIC)
@@ -29,6 +32,7 @@ public class Enjoyer extends Auditable<String> {
     //    private Long chat-botId;   for later
     private Long id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String photoUrl;
     private String about;
@@ -37,6 +41,15 @@ public class Enjoyer extends Auditable<String> {
     @Getter(AccessLevel.NONE) @Setter(AccessLevel.NONE)
     @OneToMany(targetEntity = Habit.class, cascade = CascadeType.ALL , fetch = FetchType.LAZY, mappedBy = "enjoyer")
     private List<Habit> habits = new ArrayList<>();
+
+    // Setup relationship with Friendship
+    @Getter(AccessLevel.NONE) @Setter(AccessLevel.NONE)
+    @OneToMany(targetEntity = Friendship.class, cascade = CascadeType.ALL , fetch = FetchType.LAZY, mappedBy = "requester")
+    private Set<Friendship> friendRequesters = new HashSet<>();
+
+    @Getter(AccessLevel.NONE) @Setter(AccessLevel.NONE)
+    @OneToMany(targetEntity = Friendship.class, cascade = CascadeType.ALL , fetch = FetchType.LAZY, mappedBy = "receiver")
+    private Set<Friendship> friendReceivers = new HashSet<>();
 
     public Enjoyer() {}
 
