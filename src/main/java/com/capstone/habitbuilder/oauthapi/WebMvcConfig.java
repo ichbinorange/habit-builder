2be7e651eb4cfeb4bdaf98628d7142b9543b7f05
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-02-15 01:11:04 -0800

    changed redirect url for deployment usage

--- src/main/java/com/capstone/habitbuilder/oauthapi/WebMvcConfig.java
+++ src/main/java/com/capstone/habitbuilder/oauthapi/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3001") // ("https://*.domain.com")
+                .allowedOrigins("https://ichbinorange.github.io", "https://ichbinorange.github.io/reactjs-habit-builder") // ("https://ichbinorange.github.io/reactjs-habit-builder" or for local "http://localhost:3001")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
